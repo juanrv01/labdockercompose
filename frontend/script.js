02dc38db083dcc3978f17ff540f37fac855bf414
jordanloz <jordan.lopez@correounivalle.edu.co>
2025-03-29 18:13:07 -0500

    Servicio 9 Guasca

--- frontend/script.js
+++ frontend/script.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
        /* { name: "Service 1", url: "http://localhost:5001/api" }, */
         { name: "Service 2", url: "http://localhost:5002/api" },
       /*  { name: "Service 3", url: "http://localhost:5003/api" } */
+        { name: "Service 9", url: "http://localhost:5009/api" }
     ];
 
     const list = document.getElementById("services-list");
